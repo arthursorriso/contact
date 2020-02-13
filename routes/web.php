c33ffc3204aaf28f95a1fbbcf99604a52bf6eb67
arthursorriso <arthurhgr@gmail.com>
2020-02-12 21:57:05 -0300

    Instruções no README

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ContactsController@index');
 Route::resource('contacts', 'ContactsController');
 Route::get('files/{path}/{file}', function($path = null, $file = null) {
     $path = storage_path().'/files/'.$path.'/'.$file;
